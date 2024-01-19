7f3cb9cecf441d1d74022cd4150856352cb31a48
Centi <154397079+Centypede@users.noreply.github.com>
2024-01-18 22:08:19 -0700

    Finish basic rps function

--- rps.js
+++ rps.js
@@ -1,6 +1,13 @@
+const playerWin = "Player wins!"
+const playerLose = "Computer wins!"
+const tie = "Tied! Redo!"
+
+
+let pWin = 0;
+let cWin = 0;
+
 function getComputerChoice () {
     let choice = Math.floor((Math.random() * 100) + 1)
-    console.log(choice);
     if (choice <= 33) {
         return "rock";
     }
@@ -9,5 +16,67 @@ function getComputerChoice () {
     }
     else {
         return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection, computerSelection);
+        if (playerSelection == "rock" ) {
+            switch(computerSelection) {
+                case "rock":
+                    return tie;
+                case "paper":
+                    cWin++;
+                    return playerLose;
+                case "scissors":
+                    pWin++;
+                    return playerWin;
+            }
+        }
+
+        if (playerSelection == "paper") {
+            switch(computerSelection) {
+                case "rock":
+                    pWin++;
+                    return playerWin;
+                case "paper":
+                    return tie;
+                case "scissors":
+                    cWin++;
+                    return playerLose;
+            }
+
+        }
+        
+        if (playerSelection == "scissors") {
+            switch(computerSelection) {
+                case "rock":
+                    cWin++;
+                    return playerLose;
+                case "paper":
+                    pWin++;
+                    return playerWin;
+                case "scissors":
+                    return tie;
+            }
+            
+        }
+
+        else {
+            return "Not a valid input! Try again."
+        }
+    return;
+}
+
+while (pWin < 5 && cWin < 5) {
+    let playerSelection = prompt("Rock, paper, scissors:");
+    playerSelection = playerSelection.toLowerCase();
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    if (pWin == 5) {
+        console.log("You win!");
+    }
+    if (cWin == 5) {
+        console.log("You lose!");
     }
 }
